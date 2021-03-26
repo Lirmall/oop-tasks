96ac8c2d3e350986e4e7735733d5cb2ca6466627
Lirmall <andreyk-orlangyr@mail.ru>
2021-03-26 11:10:55 +0300

    Исполнено задание №3 Также добавлено умножение матриц

--- t3/matrix/Matrix.java
+++ t3/matrix/Matrix.java
@@ -1,9 +1,9 @@
 package oop.tasks.t3.matrix;
 
 class Matrix {
-    int[][] matrix = new int[2][2];
+    private int[][] matrix = new int[2][2];
 
-        protected Matrix() {
+    public Matrix() {
         for (int i = 0; i < 2; i++) {
             for (int m = 0; m < 2; m++) {
                 this.matrix[i][m] = (int) (Math.random() * 10);
@@ -11,38 +11,51 @@ class Matrix {
         }
     }
 
-    /*
-    protected Matrix() {
-        for (int i = 0; i < 2; i++) {
-            for (int m = 0; m < 2; m++) {
-                int k = (int) (Math.random() * 10);
-                if (k == 0) {
-                    k = (int) ((Math.random() * 10) + 1);
-                }
-                this.matrix[i][m] = k ;
-            }
-        }
-     */
+    public Matrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public Matrix(int a, int b, int c, int d) {
+        matrix[0][0] = a;
+        matrix[0][1] = b;
+        matrix[1][0] = c;
+        matrix[1][1] = d;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 
     public String toString() {
         return "Matrix " + "\n" +
                 "{" + this.matrix[0][0] + "\t" + this.matrix[0][1] + "}" + "\n" +
                 "{" + this.matrix[1][0] + "\t" + this.matrix[1][1] + "}" + "\n";
     }
-/*
-    Matrix matrixSummary(Matrix matrix1) {
-        return new Matrix(
-                this.matrix[0][0] + matrix1[0][0], this.matrix[0][1] + matrix1[0][1],
-                this.matrix[1][0] + matrix1[1][0], this.matrix[1][1] + matrix1[1][1]);
+
+    public int[][] matrixSummary(int[][] matrix1) {
+        int [][] matrix2 = new int[2][2];
+        matrix2[0][0] = this.matrix[0][0] + matrix1[0][0];
+        matrix2[0][1] = this.matrix[0][1] + matrix1[0][1];
+        matrix2[1][0] = this.matrix[1][0] + matrix1[1][0];
+        matrix2[1][1] = this.matrix[1][1] + matrix1[1][1];
+        return matrix2;
     }
 
-    Matrix(Matrix matrix) {
-        for (int i = 0; i < 2; i++) {
-            for (int m = 0; m < 2; m++) {
-                this.matrix[i][m] = matrix[i][m];
-            }
-        }
+    public int[][] matrixMultiplicationByNumber (int number) {
+        int [][] matrix2 = new int[2][2];
+        matrix2[0][0] = this.matrix[0][0] * number;
+        matrix2[0][1] = this.matrix[0][1] * number;
+        matrix2[1][0] = this.matrix[1][0] * number;
+        matrix2[1][1] = this.matrix[1][1] * number;
+        return matrix2;
     }
-    */
 
+    public int[][] matricesMultiplication (int[][] matrix1) {
+        int [][] matrix2 = new int[2][2];
+        matrix2[0][0] = (this.matrix[0][0] * matrix1[0][0]) + (this.matrix[0][1] * matrix1[1][0]);
+        matrix2[0][1] = (this.matrix[0][0] * matrix1[0][1]) + (this.matrix[0][1] * matrix1[1][1]);
+        matrix2[1][0] = (this.matrix[1][0] * matrix1[0][0]) + (this.matrix[1][1] * matrix1[1][0]);
+        matrix2[1][1] = (this.matrix[1][0] * matrix1[0][1]) + (this.matrix[1][1] * matrix1[1][1]);
+        return matrix2;
+    }
 }
--- t3/matrix/MatrixRun.java
+++ t3/matrix/MatrixRun.java
@@ -4,14 +4,28 @@ public class MatrixRun {
     public static void main(String[] args) {
         Matrix matrix1 = new Matrix();
         Matrix matrix2 = new Matrix();
+        Matrix matrix3 = new Matrix(matrix1.getMatrix());
 
+        System.out.println("matrix1");
         System.out.println(matrix1.toString());
+        System.out.println("matrix2");
         System.out.println(matrix2.toString());
+        System.out.println("matrix3 - clone matrix1");
+        System.out.println(matrix3.toString());
 
-/*
-        Matrix resultSummary = matrix1.matrixSummary(matrix2);
-        System.out.println(matrix2.toString(resultSummary));
-*/
+        Matrix resultSummary = new Matrix(matrix1.matrixSummary(matrix2.getMatrix()));
+        System.out.println("matrix1 + matrix2");
+        System.out.println(resultSummary.toString());
+        System.out.println();
+
+        Matrix resultMatrixMultiplicationByNumber = new Matrix(matrix1.matrixMultiplicationByNumber(2));
+        System.out.println("matrix1 * number");
+        System.out.println(resultMatrixMultiplicationByNumber.toString());
+        System.out.println();
+
+        Matrix resultMatricesMultiplication = new Matrix(matrix1.matricesMultiplication(matrix2.getMatrix()));
+        System.out.println("matrix1 * matrix2");
+        System.out.println(resultMatricesMultiplication.toString());
+        System.out.println();
     }
-
 }
