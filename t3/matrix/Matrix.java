7dff51048216360cf173282f077558b197b0957b
Lirmall <andreyk-orlangyr@mail.ru>
2021-04-02 13:29:44 +0300

    Задание 3 исправлено кроме константы

--- t3/matrix/Matrix.java
+++ t3/matrix/Matrix.java
@@ -1,12 +1,13 @@
 package oop.tasks.t3.matrix;
 
 class Matrix {
-    private int[][] matrix = new int[2][2];
+    final int HEIGHT = 2;
+    private int[][] matrix = new int[HEIGHT][HEIGHT];
 
     public Matrix() {
-        for (int i = 0; i < 2; i++) {
-            for (int m = 0; m < 2; m++) {
-                this.matrix[i][m] = (int) (Math.random() * 10);
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                this.matrix[i][j] = (int) (Math.random() * 10);
             }
         }
     }
@@ -15,11 +16,11 @@ class Matrix {
         this.matrix = matrix;
     }
 
-    public Matrix(int a, int b, int c, int d) {
-        matrix[0][0] = a;
-        matrix[0][1] = b;
-        matrix[1][0] = c;
-        matrix[1][1] = d;
+    public Matrix(int a11, int a12, int a21, int a22) {
+        matrix[0][0] = a11;
+        matrix[0][1] = a12;
+        matrix[1][0] = a21;
+        matrix[1][1] = a22;
     }
 
     public int[][] getMatrix() {
@@ -28,30 +29,32 @@ class Matrix {
 
     public String toString() {
         return "Matrix " + "\n" +
-                "{" + this.matrix[0][0] + "\t" + this.matrix[0][1] + "}" + "\n" +
-                "{" + this.matrix[1][0] + "\t" + this.matrix[1][1] + "}" + "\n";
+                "(" + this.matrix[0][0] + "\t" + this.matrix[0][1] + ")" + "\n" +
+                "(" + this.matrix[1][0] + "\t" + this.matrix[1][1] + ")" + "\n";
     }
 
     public int[][] matrixSummary(int[][] matrix1) {
-        int [][] matrix2 = new int[2][2];
-        matrix2[0][0] = this.matrix[0][0] + matrix1[0][0];
-        matrix2[0][1] = this.matrix[0][1] + matrix1[0][1];
-        matrix2[1][0] = this.matrix[1][0] + matrix1[1][0];
-        matrix2[1][1] = this.matrix[1][1] + matrix1[1][1];
+        int [][] matrix2 = new int[HEIGHT][HEIGHT];
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                matrix2[i][j] = this.matrix[i][j] + matrix1[i][j];
+            }
+        }
         return matrix2;
     }
 
-    public int[][] matrixMultiplicationByNumber (int number) {
-        int [][] matrix2 = new int[2][2];
-        matrix2[0][0] = this.matrix[0][0] * number;
-        matrix2[0][1] = this.matrix[0][1] * number;
-        matrix2[1][0] = this.matrix[1][0] * number;
-        matrix2[1][1] = this.matrix[1][1] * number;
+    public int[][] matrixMultiplicationByNumber(int number) {
+        int [][] matrix2 = new int[HEIGHT][HEIGHT];
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                matrix2[i][j] = this.matrix[i][j] * number;
+            }
+        }
         return matrix2;
     }
 
     public int[][] matricesMultiplication (int[][] matrix1) {
-        int [][] matrix2 = new int[2][2];
+        int [][] matrix2 = new int[HEIGHT][HEIGHT];
         matrix2[0][0] = (this.matrix[0][0] * matrix1[0][0]) + (this.matrix[0][1] * matrix1[1][0]);
         matrix2[0][1] = (this.matrix[0][0] * matrix1[0][1]) + (this.matrix[0][1] * matrix1[1][1]);
         matrix2[1][0] = (this.matrix[1][0] * matrix1[0][0]) + (this.matrix[1][1] * matrix1[1][0]);
--- t3/matrix/MatrixRun.java
+++ t3/matrix/MatrixRun.java
@@ -6,6 +6,15 @@ public class MatrixRun {
         Matrix matrix2 = new Matrix();
         Matrix matrix3 = new Matrix(matrix1.getMatrix());
 
+        Matrix matrixT1 = new Matrix(1, 2, 3, 4);
+        Matrix matrixT2 = new Matrix(9, 8, 7, 6);
+
+        System.out.println("matrixT1");
+        System.out.println(matrixT1);
+        System.out.println("matrixT2");
+        System.out.println(matrixT2);
+        System.out.println();
+
         System.out.println("matrix1");
         System.out.println(matrix1.toString());
         System.out.println("matrix2");
@@ -18,14 +27,29 @@ public class MatrixRun {
         System.out.println(resultSummary.toString());
         System.out.println();
 
+        Matrix resultSummary2 = new Matrix(matrixT1.matrixSummary(matrixT2.getMatrix()));
+        System.out.println("matrixT1 + matrixT2");
+        System.out.println(resultSummary2.toString());
+        System.out.println();
+
         Matrix resultMatrixMultiplicationByNumber = new Matrix(matrix1.matrixMultiplicationByNumber(2));
         System.out.println("matrix1 * number");
         System.out.println(resultMatrixMultiplicationByNumber.toString());
         System.out.println();
 
+        Matrix resultMatrixMultiplicationByNumber2 = new Matrix(matrixT1.matrixMultiplicationByNumber(2));
+        System.out.println("matrixT1 * number");
+        System.out.println(resultMatrixMultiplicationByNumber2.toString());
+        System.out.println();
+
         Matrix resultMatricesMultiplication = new Matrix(matrix1.matricesMultiplication(matrix2.getMatrix()));
         System.out.println("matrix1 * matrix2");
         System.out.println(resultMatricesMultiplication.toString());
+        System.out.println();
+
+        Matrix resultMatricesMultiplication2 = new Matrix(matrixT1.matricesMultiplication(matrixT2.getMatrix()));
+        System.out.println("matrixT1 * matrixT2");
+        System.out.println(resultMatricesMultiplication2.toString());
         System.out.println();
     }
 }
