9394e3712288d4a0d2c32094933ebf99a5467b11
Lirmall <andreyk-orlangyr@mail.ru>
2021-06-03 12:58:12 +0300

    Task 1 Strings

---
+++ string1/StringMethodsPractice.java
@@ -0,0 +1,13 @@
+package oop.tasks.string1;
+
+public class StringMethodsPractice {
+    private final String string;
+
+    public StringMethodsPractice(String string) {
+        this.string = string;
+    }
+
+    public String getString() {
+        return string;
+    }
+}
