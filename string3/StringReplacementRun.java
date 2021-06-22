01f7fa2d208123cbe3000d685cd50ca4787deb44
Lirmall <andreyk-orlangyr@mail.ru>
2021-06-22 11:33:12 +0300

    Task 3 Strings

---
+++ string3/StringReplacementRun.java
@@ -0,0 +1,22 @@
+package oop.tasks.string3;
+
+public class StringReplacementRun {
+    public static void main(String[] args) {
+
+        StringReplacement srp = new StringReplacement("Object-oriented programming is a programming language model " +
+                " organized around objects rather than actions and data rather than logic. " + "\n" +
+                "Object-oriented programming blabla. Object-oriented programming bla. Object-oriented programming bla-bla");
+
+        String str2 = "Object-oriented programming";
+        String str3 = "OOP";
+
+        String str1 = srp.getString();
+        System.out.println(str1 + "\n");
+        System.out.println(srp.getString().length());
+
+        System.out.println(srp.replacement1("Object-oriented programming", "OOP") + "\n");
+
+        System.out.println(srp.replacement1(str2, str3));
+    }
+
+}
