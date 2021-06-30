5fb3484331bcdad2282091ee5d4e4edaddab8edd
Lirmall <andreyk-orlangyr@mail.ru>
2021-06-30 17:10:14 +0300

    Task 11 Strings

---
+++ string11/WordCounter.java
@@ -0,0 +1,19 @@
+package oop.tasks.string11;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WordCounter {
+    public static void main(String[] args) {
+        int i = 0;
+        String str = "One two three раз два три one1 two2 123 ";
+        Pattern pattern = Pattern.compile("[a-z]+\\s", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(str);
+
+        while (matcher.find()) {
+            i++;
+        }
+
+        System.out.println(i);
+    }
+}
