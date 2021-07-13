aaa2c4315ed0e65768918201cda82894f360c866
Lirmall <andreyk-orlangyr@mail.ru>
2021-07-13 15:48:21 +0300

    Task 2 Collections

--- collections2/HeavyBox.java
+++ collections2/HeavyBox.java
@@ -1,11 +1,11 @@
 package oop.tasks.collections2;
 
 public class HeavyBox {
-    String name;
-    double length;
-    double width;
-    double height;
-    double weight;
+    private String name;
+    private double length;
+    private double width;
+    private double height;
+    private double weight;
 
     public HeavyBox(String name, double length, double width, double height, double weight) {
         this.name = name;
